6a1a4d27795a768e545017f4e3047814c8f6ad5d
josephlee01 <bluewings113@gmail.com>
2022-07-05 11:10:49 +0800

    #17

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.4;
-var showResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, showResult, resultPhrase);
+var person = {
+    name: 'Joseph',
+    age: 35
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+const person: {
+  name:string,
+  age:number
+} = {
+  name: "Joseph",
+  age: 35,
+};
 
-const number1 = 5;
-const number2 = 2.4;
-const showResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, showResult, resultPhrase);
+console.log(person.name);
