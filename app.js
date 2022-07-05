c32a81b89fdb523259ad8e4c8dba038bcb3c74bf
josephlee01 <bluewings113@gmail.com>
2022-07-05 23:58:35 +0800

    #19

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// const person: {
+//   name:string,
+//   age:number,
+//   hobbies: string[]
+// } = {
 var person = {
-    name: 'Joseph',
-    age: 35
+    name: "Joseph",
+    age: 35,
+    hobbies: ['go for a drive', 'swimming', 'making coffee'],
+    role: [2, 'author']
 };
-console.log(person);
+person.role[0] = 5;
+person.role.push('admin');
+console.log(person.name);
+console.log(person.role);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,22 @@
 const person: {
   name:string,
-  age:number
+  age:number,
+  hobbies: string[],
+  role:  [number, string]
 } = {
   name: "Joseph",
   age: 35,
+  hobbies: ['go for a drive', 'swimming', 'making coffee'],
+  role: [2, 'author']
 };
 
+person.role[1] = 5 // role 배열의 두번째는 string 이어야 함.
+person.role = [3, "police"] // number, string 순서 맞으니까 가능.
+person.role.push('admin') // 근데 왜 push는 되는거야?
+
 console.log(person.name);
+console.log(person.role);
+
+for (const hobby of person.hobbies){
+  console.log(hobby.toUpperCase())
+}
