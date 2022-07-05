d5dbb0ed139ea4c29e1a4b9aad685b46c80226f1
josephlee01 <bluewings113@gmail.com>
2022-07-06 00:58:57 +0800

    #22

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-// enum은 객체에 순번을 부여함
-var Level1;
-(function (Level1) {
-    Level1[Level1["admin"] = 0] = "admin";
-    Level1[Level1["readOnly"] = 1] = "readOnly";
-    Level1[Level1["author"] = 2] = "author";
-})(Level1 || (Level1 = {}));
-var Level2;
-(function (Level2) {
-    Level2["admin"] = "ADMIN";
-    Level2[Level2["readOnly"] = 100] = "readOnly";
-    Level2[Level2["author"] = 999] = "author";
-})(Level2 || (Level2 = {}));
-var person = {
-    name: "Joseph",
-    age: 35,
-    hobbies: ["go for a drive", "swimming", "making coffee"],
-    role: [2, "author"],
-    level: Level1.readOnly
-};
-var person2 = {
-    level: Level2.author
-};
-// person.role[1] = 5; // role 배열의 두번째는 string 이어야 함.
-person.role = [3, "police"]; // number, string 순서 맞으니까 가능.
-person.role.push("admin"); // 근데 왜 push는 되는거야?
-console.log(person.name);
-console.log(person.role);
-console.log("Person1 Access Level = ".concat(person.level));
-console.log("Person2 Access Level = ".concat(person2.level));
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combinedAges = combine(30, 35);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,48 +1,17 @@
-// enum은 객체에 순번을 부여함
-enum Level1 {
-  admin, // 0
-  readOnly, // 1
-  author, // 2
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-enum Level2 {
-  admin = "ADMIN",
-  readOnly = 100,
-  author = 999,
-}
+const combinedAges = combine(30, 35);
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-  level: Level1;
-} = {
-  name: "Joseph",
-  age: 35,
-  hobbies: ["go for a drive", "swimming", "making coffee"],
-  role: [2, "author"],
-  level: Level1.readOnly,
-};
+console.log(combinedAges);
 
-const person2 = {
-  level: Level2.author,
-};
+const combinedNames = combine("Max", "Anna");
 
-// person.role[1] = 5; // role 배열의 두번째는 string 이어야 함.
-person.role = [3, "police"]; // number, string 순서 맞으니까 가능.
-person.role.push("admin"); // 근데 왜 push는 되는거야?
-
-console.log(person.name);
-console.log(person.role);
-console.log(`Person1 Access Level = ${person.level}`);
-console.log(`Person2 Access Level = ${person2.level}`);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-
-// any
-let favWords: any[];
-favWords = [1, "blue", false, null, undefined];
+console.log(combinedNames);
