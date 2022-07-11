067a13c1430a5fc3c63d59566e27d11e4d02c793
josephlee01 <bluewings113@gmail.com>
2022-07-11 20:38:39 +0800

    #35

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-// unknown은 any보다 제한적임.
-// but 2-3개의 타입이 예상된다면 || 를 사용할 것
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-// never
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An error occurred!', 500);
+"use strict";
+console.log('Sending app.ts ...');
---
+++ unknown-never.ts
@@ -0,0 +1,19 @@
+// unknown은 any보다 제한적임.
+// but 2-3개의 타입이 예상된다면 || 를 사용할 것
+
+let userInput: unknown
+let userName: string
+
+userInput = 5
+userInput = 'Max'
+if(typeof userInput === 'string'){
+  userName = userInput
+} 
+
+
+// never (void도 가능)
+function generateError(message: string, code: number):never{
+  throw {message, errorCode: code}
+}
+
+generateError('An error occurred!', 500) 
