8f6782df73ab3375210e4c5e7b0596b1b66286a5
josephlee01 <bluewings113@gmail.com>
2022-07-12 23:58:38 +0800

    #50

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,9 @@
 "use strict";
-function add(a, b) {
-    let result;
-    result = a + b;
-    return result;
+const add = (a, b) => a + b;
+console.log(add(2, 8));
+const printOutput = (output) => console.log(output);
+const button = document.querySelector("button");
+if (button) {
+    button.addEventListener("click", (event) => console.log(event));
 }
+printOutput(add(5, 2));
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
-function add(a:number,b:number){
-  let result
-  result = a+b
-  return result
+// function add(a:number,b:number){
+//   let result
+//   result = a+b
+//   return result
+// }
+// console.log(result)
+
+
+
+
+
+// Arrow Func.
+
+// const add = (a:number,b:number) => {
+//   return a+b
+// }
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 8));
+
+// const printOutput = (output: string | number) => console.log(output)
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
 }
 
-// console.log(result)
+printOutput(add(5, 2));
