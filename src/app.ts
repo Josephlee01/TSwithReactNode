3b844df6ed88f33f623e4efbc48af25be04cca9f
josephlee01 <bluewings113@gmail.com>
2022-07-19 03:37:38 +0800

    #61

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,15 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department("Accounting");
 
-console.log(accounting)
+accounting.describe();
+
+const accountingCopy = { name: "Dummy", describe: accounting.describe };
+
+accountingCopy.describe()
