0ca359e8e19ee7cd5d3c1339799846cac7d17a66
josephlee01 <bluewings113@gmail.com>
2022-07-20 00:12:42 +0800

    #63

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
 class Department {
-  public name: string;
+  // private id: string;
+  // public name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.name = n;
   }
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department: [${this.id}] ${this.name} `);
   }
   addEmployee(employee: string) {
     this.employees.push(employee);
   }
-  printEmployeeInfo(){
-    console.log(this.employees.length)
-    console.log(this.employees)
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-
-const accounting = new Department("Accounting");
-accounting.addEmployee('Max')
-accounting.addEmployee('Joe')
-// accounting.employees[2] = 'Manu' => private이기 때문에 작동 안함.
+const accounting = new Department("D1", "Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Joe");
+// accounting.employees[2] = 'Manu' => private이기 때문에 추가안됨.
 
 accounting.describe();
-accounting.printEmployeeInfo()
+accounting.printEmployeeInfo();
 
 // const accountingCopy = { name: "Dummy", describe: accounting.describe };
 // accountingCopy.describe();
