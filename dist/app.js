00523a98ee955ecec1b5df263231f780c4e39e59
josephlee01 <bluewings113@gmail.com>
2022-07-27 23:29:30 +0800

    #77

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,8 @@
 "use strict";
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
 class Person {
     constructor(n) {
         this.age = 35;
@@ -10,5 +14,5 @@ class Person {
 }
 let user1;
 user1 = new Person("Joe");
-user1.greet('Hi there, ');
+user1.greet("Hi there, ");
 console.log(user1);
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,41 @@
+// type addFn = (a: number, b: number) => number;
+interface addFn {
+  (a: number, b: number): number
+}
+
+let add: addFn;
+
+add = (n1, n2) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string
+  readonly name: string;
 }
 
 // 두개의 interface 병합 가능
 interface Greetable extends Named {
-
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
   name: string;
-  age= 35;
-  
-  constructor(n:string) {
-    this.name= n
+  age = 35;
+
+  constructor(n: string) {
+    this.name = n;
   }
   // interface는 implements에 greet() 가 있어야 함을 강제함.
-  greet(phrase:string) {
+  greet(phrase: string) {
     console.log(phrase + "" + this.name);
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person("Joe")
-// user1.name = 'Henry' 
+user1 = new Person("Joe");
+// user1.name = 'Henry'
 // ⬆️ readonly이기 때문에 불가능
 
-user1.greet('Hi there, ')
-console.log(user1)
+user1.greet("Hi there, ");
+console.log(user1);
