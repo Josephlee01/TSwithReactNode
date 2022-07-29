9cb3b3f6cd54705420cf20416795f6b80963b8b0
josephlee01 <bluewings113@gmail.com>
2022-07-29 22:59:28 +0800

    #84

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ type Employee = {
 
 type ElevatedEmployee = Admin & Employee;
 
-// interface 사용 시 
+// interface 사용 시
 // interface Admin {
 //   name: string;
 //   privileges: string[];
@@ -25,15 +25,64 @@ type ElevatedEmployee = Admin & Employee;
 
 // interface ElevatedEmployee extends Admin, Employee {}
 
-
 const e1: ElevatedEmployee = {
   name: "Joe",
   privileges: ["create-server"],
   startDate: new Date(),
 };
 
-type Combinable = string | number
-type Numeric = number | boolean
-type Universal = Combinable & Numeric
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
 
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
 
+type UnKnownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnKnownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading Cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const car1 = new Car();
+const car2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(car1);
+useVehicle(car2);
