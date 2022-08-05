7a8023dff43ce6b3e31c2c9cd5d53c3a542ea0c5
josephlee01 <bluewings113@gmail.com>
2022-08-05 22:34:15 +0800

    #95

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 "use strict";
 const names = ["Joe", "Max", "Ellie"];
-console.log(names[2].split(''));
+console.log(names[2].split(""));
 const fruits = [];
 fruits[0] = "Cranberry";
 const promise = new Promise((resolve, reject) => {
@@ -9,3 +9,9 @@ const promise = new Promise((resolve, reject) => {
     }, 1000);
 });
 promise.then((data) => data.split(""));
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: "Joe", hobbies: ['Sports'] }, { age: 35 });
+console.log(mergedObj.name);
+console.log(mergedObj.hobbies);
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // 추론
 const names = ["Joe", "Max", "Ellie"];
-console.log(names[2].split(''))
+console.log(names[2].split(""));
 
 // Generics
 const fruits: Array<string> = [];
@@ -13,3 +13,11 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 });
 
 promise.then((data) => data.split(""));
+
+function merge<T,U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Joe", hobbies: ['Sports'] }, { age: 35 });
+console.log(mergedObj.name)
+console.log(mergedObj.hobbies)
